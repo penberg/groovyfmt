5ddd7a9cef9406f0bca8d51bb15c5b63dff1d2a5
Pekka Enberg <penberg@iki.fi>
2020-02-04 18:09:00 +0200

    Wire up Groovy's SourcePrinter

--- src/main/java/org/penberg/groovyfmt/Main.java
+++ src/main/java/org/penberg/groovyfmt/Main.java
@@ -1,11 +1,17 @@
 package org.penberg.groovyfmt;
 
 import antlr.collections.AST;
+import java.io.ByteArrayOutputStream;
 import java.io.FileReader;
+import java.io.PrintStream;
+import org.codehaus.groovy.antlr.AntlrASTProcessor;
 import org.codehaus.groovy.antlr.SourceBuffer;
 import org.codehaus.groovy.antlr.UnicodeEscapingReader;
 import org.codehaus.groovy.antlr.parser.GroovyLexer;
 import org.codehaus.groovy.antlr.parser.GroovyRecognizer;
+import org.codehaus.groovy.antlr.treewalker.SourceCodeTraversal;
+import org.codehaus.groovy.antlr.treewalker.SourcePrinter;
+import org.codehaus.groovy.antlr.treewalker.Visitor;
 
 public class Main {
   public static void main(String[] args) throws Exception {
@@ -16,8 +22,13 @@ public class Main {
     GroovyLexer lexer = new GroovyLexer(unicodeReader);
     unicodeReader.setLexer(lexer);
     GroovyRecognizer parser = GroovyRecognizer.make(lexer);
+    String[] tokenNames = parser.getTokenNames();
     parser.setSourceBuffer(sourceBuffer);
     parser.compilationUnit();
     AST ast = parser.getAST();
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    Visitor visitor = new SourcePrinter(System.out, tokenNames, true);
+    AntlrASTProcessor traverser = new SourceCodeTraversal(visitor);
+    traverser.process(ast);
   }
 }
