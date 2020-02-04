b0d4e6bfe56d2df3105ab8314c15eb44afa07848
Pekka Enberg <penberg@iki.fi>
2020-02-04 17:45:04 +0200

    Wire up ANTLR-generated parser

--- src/main/java/org/penberg/groovyfmt/Main.java
+++ src/main/java/org/penberg/groovyfmt/Main.java
@@ -1,8 +1,23 @@
 package org.penberg.groovyfmt;
 
+import antlr.collections.AST;
+import java.io.FileReader;
+import org.codehaus.groovy.antlr.SourceBuffer;
+import org.codehaus.groovy.antlr.UnicodeEscapingReader;
+import org.codehaus.groovy.antlr.parser.GroovyLexer;
 import org.codehaus.groovy.antlr.parser.GroovyRecognizer;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    System.out.println("Formatting " + args[0] + " ...");
+    SourceBuffer sourceBuffer = new SourceBuffer();
+    FileReader reader = new FileReader(args[0]);
+    UnicodeEscapingReader unicodeReader = new UnicodeEscapingReader(reader, sourceBuffer);
+    GroovyLexer lexer = new GroovyLexer(unicodeReader);
+    unicodeReader.setLexer(lexer);
+    GroovyRecognizer parser = GroovyRecognizer.make(lexer);
+    parser.setSourceBuffer(sourceBuffer);
+    parser.compilationUnit();
+    AST ast = parser.getAST();
   }
 }
